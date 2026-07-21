c56684fbea159901f4aacd42d2877170df973fe2
Remah Shayya <rimahshaya@gmail.com>
2026-07-21 15:33:18 +0300

    Point frontend at deployed backend

--- Nova Frontend/js/config.js
+++ Nova Frontend/js/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://nova-social-media-platform.onrender.com';
 const API_URL = BASE_URL + '/api';
 const TOKEN_KEY = 'nova_token';
 const USER_KEY = 'nova_user';
